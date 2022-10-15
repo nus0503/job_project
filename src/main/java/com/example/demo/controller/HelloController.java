1084aedb5084bfcd92d42fee37fbe4929b4686f4
kjs <nus0503@naver.com>
2022-10-15 18:03:12 +0900

    2022-10-15

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(Model model) {
+
+        model.addAttribute("name", "345hi");
+
+        return "hello";
+    }
+}
