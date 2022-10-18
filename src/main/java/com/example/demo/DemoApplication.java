d99422b012ca367dce31e050f7d9d14426875b46
kjs <nus0503@naver.com>
2022-10-18 19:06:57 +0900

    2022-10-18

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,21 @@ package com.example.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@MapperScan(value = {com.example.demo.mapper})
-
+//@MapperScan(value = {"com.example.demo.mapper"})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		Test test = new Test();
+		test.setUserName("홍길동");
+		String data = test.getUserName();
+		System.out.println(data);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Test {
+    private String userName;
+}
