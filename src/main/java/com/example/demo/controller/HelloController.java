1e6af12363ac79cc0aa95cc23826037d69915001
kjs <nus0503@naver.com>
2022-10-19 19:21:26 +0900

    2022-10-19

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+
+    private final Logger logger = LoggerFactory.getLogger("LoggerController 의 로그");
 
     @RequestMapping("/")
     public String index() {
@@ -18,5 +23,10 @@ public class HelloController {
     public String login() {
 
         return "login";
+    }
+
+    @GetMapping("/og")
+    public void log() {
+        logger.info("로깅 발생sdsdd!");
     }
 }
